1abbe7f83bbcaf2711bf64d121cc38990c9c44cc
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-21 18:52:51 +0530

    classes and objects..

--- trial.py
+++ trial.py
@@ -1,1 +1,27 @@
 # practice section...
+# Inheritance...
+
+# Declarating Classes...
+class sum():
+    def __init__(self):
+        print("Init 1")
+    def add1(self):
+        print('Addition 1')
+    def add2(self):
+        print('Addition 2')
+
+class sub(sum):
+    def __init__(self):
+        print("Init 2")
+    def sub1(self):
+        super().add1()
+        print('Subtraction 1')
+    def subt2(self):
+        print('Subtraction 2')
+
+# Oject Declarating...
+obj = sub()
+obj1 = sub()
+
+# Calling Methods...
+obj1.sub1()
