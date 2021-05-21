9c042a5909a272dbfb4df04fb9360b3e4cca5370
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-22 02:29:50 +0530

    File Handelling practice...

--- trial.py
+++ trial.py
@@ -1,27 +1,8 @@
 # practice section...
-# Inheritance...
 
-# Declarating Classes...
-class sum():
-    def __init__(self):
-        print("Init 1")
-    def add1(self):
-        print('Addition 1')
-    def add2(self):
-        print('Addition 2')
+f1 = open('my_data',"a")
+#f1.write("1234567890")
+f2 = open('trial.py','r')
 
-class sub(sum):
-    def __init__(self):
-        print("Init 2")
-    def sub1(self):
-        super().add1()
-        print('Subtraction 1')
-    def subt2(self):
-        print('Subtraction 2')
-
-# Oject Declarating...
-obj = sub()
-obj1 = sub()
-
-# Calling Methods...
-obj1.sub1()
+for data in f2:
+    f1.write(data)
