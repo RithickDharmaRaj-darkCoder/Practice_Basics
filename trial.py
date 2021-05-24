c594c93d1ed272d9154c0e2ef42e9505ed5e9ac0
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-25 02:08:11 +0530

    Bubble sort added...

--- trial.py
+++ trial.py
@@ -1,12 +1,1 @@
 # practice section...
-
-def bubblesort():
-    lst = []
-    lsize = int(input("\nEnter how many elements to be insert : "))
-    for i in range(lsize):
-        elst = input("Enter the elements to add it in list : ")
-        lst.append(elst)
-    print(lst)
-
-    for out in range()
-bubblesort()
