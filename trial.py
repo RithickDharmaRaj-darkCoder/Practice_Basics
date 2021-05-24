7ebc3a8eae3db2078329b08cc9e839cbdec49650
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-25 01:40:20 +0530

    l & R File name changed...

--- trial.py
+++ trial.py
@@ -1,2 +1,12 @@
 # practice section...
 
+def bubblesort():
+    lst = []
+    lsize = int(input("\nEnter how many elements to be insert : "))
+    for i in range(lsize):
+        elst = input("Enter the elements to add it in list : ")
+        lst.append(elst)
+    print(lst)
+
+    for out in range()
+bubblesort()
