b1866e484af0e980e0cec613f18eccc32d3ebab9
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-27 17:02:04 +0530

    Pattern 17 Added...

--- trial.py
+++ trial.py
@@ -4,8 +4,8 @@ sq = input("Enter one element : ").upper()
 # r = int(input("Enter no.of rows : "))
 for row in range(5):
     print("    ", end="")
-    for space in range(5 - row - 1):
+    for space in range(5-row-1):
         print(" ", end=" ")
-    for column in range(row + 1):
-        print(sq+"  ", end=" ")
+    for column in range(row+1):
+        print(sq+"", end=" ")
     print()
