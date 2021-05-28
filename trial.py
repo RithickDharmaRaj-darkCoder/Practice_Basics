25a6fc001d34b8e98206eefe9b17a81eff4c5217
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-29 03:27:47 +0530

    Letter 'B' pattern added...

--- trial.py
+++ trial.py
@@ -1,10 +1,11 @@
 # practice section...
 
+sq = input("Enter one element : ").upper()
 for row in range(7):
-    print("    ",end="")
+    print("    ", end="")
     for column in range(5):
-        if ((column == 0 or column == 4) and row != 0)  or ((row == 0 or row == 3) and (column > 0 and column < 4)):
-            print(" * ",end="")
+        if ( column == 0 ) or ( column == 4 and ( row != 0 and row != 3 and row != 6)) or ( (row == 0 or row == 3 or row == 6) and ( 0 < column  and column < 4)) :
+            print(f" {sq} ",end="")
         else:
             print(end="   ")
     print()
