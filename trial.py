e039a06395659f583a92c15fa445364f2d86ec5c
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-30 03:35:53 +0530

    Letter 'K' Added...

--- trial.py
+++ trial.py
@@ -2,11 +2,19 @@
 
 sq = input("Enter one element : ").upper()
 if len(sq) == 1:
+    r = 0
+    c = 4
     for row in range(7):
         print("    ", end="")
         for column in range(5):
-            if (column == 2) or (row == 0) or (row == 6 and column < 2) or (column == 0 and row == 5):
+            if (column == 0):
                 print(f" {sq} ",end="")
+            elif (row == column+2 and column > 1):
+                print(f" {sq} ", end="")
+            elif (row == r and column == c):
+                print(f" {sq} ", end="")
+                r += 1
+                c -= 1
             else:
                 print(end="   ")
         print()
