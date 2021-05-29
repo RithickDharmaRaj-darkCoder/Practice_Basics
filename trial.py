471bb2a6de2b2771cc1d1e3f6135b5be44cd2dfd
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-29 14:06:46 +0530

    Letter 'F' pattern added...

--- trial.py
+++ trial.py
@@ -4,7 +4,7 @@ sq = input("Enter one element : ").upper()
 for row in range(7):
     print("    ", end="")
     for column in range(5):
-        if (column == 0) or ((row == 0 or row == 3 or row == 6) and (0 < column)):
+        if (column == 0) or ((row == 0 or row == 3) and (column > 0)):
             print(f" {sq} ",end="")
         else:
             print(end="   ")
