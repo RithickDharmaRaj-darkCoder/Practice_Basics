72244a71b08f25da2949914de80a7a55c37f36c6
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-30 03:05:56 +0530

    Intentation Error overcame ...

--- trial.py
+++ trial.py
@@ -1,11 +1,14 @@
 # practice section...
 
 sq = input("Enter one element : ").upper()
-for row in range(7):
-    print("    ", end="")
-    for column in range(5):
-        if (column == 2) or (row == 0 or row == 6):
-            print(f" █ ",end="")
-        else:
-            print(end="   ")
-    print()
+if len(sq) == 1:
+    for row in range(7):
+        print("    ", end="")
+        for column in range(5):
+            if (column == 2) or (row == 0) or (row == 6 and column < 2) or (column == 0 and row == 5):
+                print(f" {sq} ",end="")
+            else:
+                print(end="   ")
+        print()
+else:
+    print("Warning! Enter only '1' Character.")
