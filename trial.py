2dbde2e132468d4f101ebf665e200267388e4bc4
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-29 13:50:28 +0530

    Letter 'E' pattern added...

--- trial.py
+++ trial.py
@@ -4,7 +4,7 @@ sq = input("Enter one element : ").upper()
 for row in range(7):
     print("    ", end="")
     for column in range(5):
-        if (column == 1) or (column == 4 and (row != 0 and row != 6)) or ((row == 0 or row == 6) and (column != 4 and column != 6) ):
+        if (column == 0) or ((row == 0 or row == 3 or row == 6) and (0 < column)):
             print(f" {sq} ",end="")
         else:
             print(end="   ")
