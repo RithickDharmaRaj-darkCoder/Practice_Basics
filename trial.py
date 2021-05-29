bdbf720be2738622f2c1af118679c94f8fff593b
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-30 02:23:28 +0530

    Letter 'H' pattern added...

--- trial.py
+++ trial.py
@@ -4,7 +4,7 @@ sq = input("Enter one element : ").upper()
 for row in range(7):
     print("    ", end="")
     for column in range(5):
-        if (column == 0 ) or (column == 4 and  ( row != 1 and row != 2 and row != 3)) or (row == 0 and (0 < column and column < 4 )) or (row == 4 and column != 1) or (row ==5 and column == 2) or (row == 6 and (0 < column and column < 3)):
+        if (column == 0 or column == 4) or (row == 3):
             print(f" {sq} ",end="")
         else:
             print(end="   ")
