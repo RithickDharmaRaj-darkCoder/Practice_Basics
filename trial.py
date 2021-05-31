fb081c0f0d30141b6c1d103278a742e63bcee04d
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-31 21:37:54 +0530

    Letter 'T' Added...

--- trial.py
+++ trial.py
@@ -5,11 +5,9 @@ if len(sq) == 1:
     for row in range(7):
         print("    ", end="")
         for column in range(5):
-            if (row == 0 or row == 3 or row == 6) and (column != 0 and column != 4):
+            if (row == 0):
                 print(f" {sq} ", end="")
-            elif (column == 0 and 0 < row < 3):
-                print(f" {sq} ", end="")
-            elif (column == 4 and 3 < row < 6) :
+            elif (column == 2):
                 print(f" {sq} ", end="")
             else:
                 print(end="   ")
