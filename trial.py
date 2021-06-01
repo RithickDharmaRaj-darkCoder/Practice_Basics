6648fcb687ab213ec4e8171ec0444c56bead0e13
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-02 00:43:31 +0530

    Pattern 24 is added...

--- trial.py
+++ trial.py
@@ -1,10 +1,15 @@
 # String Methods...
 
-# sq = int(input("How many rows needed? : "))
-c = 1
-for row in range(5):
-    print("     ", end=" ")
-    for column in range(1,c+1):
-        print('*',end=" ")
-    c += 2
-    print()
+sq = input("Enter one element : ").upper()
+if len(sq) == 1:
+    # r = int(input("Enter no.of rows : "))
+    c = 1
+    for row in range(5):
+        print("    ", end="")
+        for space in range(5 - row - 1):
+            print(end="   ")
+        for column in range(2*row+1):
+            print(sq , end="  ")
+        print()
+else:
+    print("Warning! Enter only '1' Character.")
