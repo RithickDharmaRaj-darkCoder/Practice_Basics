c4b2f74bb73ea26c4a4a756163a778981833a67c
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-02 01:51:19 +0530

    Pattern 25 is added...

--- trial.py
+++ trial.py
@@ -2,14 +2,18 @@
 
 sq = input("Enter one element : ").upper()
 if len(sq) == 1:
+    print()
     # r = int(input("Enter no.of rows : "))
-    c = 1
-    for row in range(5):
+    c = 0
+    s = 9
+    for row in range(s):
         print("    ", end="")
-        for space in range(5 - row - 1):
-            print(end="   ")
-        for column in range(2*row+1):
-            print(sq , end="  ")
+        for space in range(9 - s):
+            print(end="  ")
+        for column in range(9-c):
+            print(sq+" ", end="  ")
+        c += 2
+        s -= 2
         print()
 else:
     print("Warning! Enter only '1' Character.")
