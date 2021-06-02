1e52f0f319721e0e30dbdc6666ec70e0c93bc480
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-02 14:03:04 +0530

    Pattern 27 is added...

--- trial.py
+++ trial.py
@@ -1,16 +1,17 @@
 # String Methods...
 
-sq = input("Enter one element : ").upper()
-if len(sq) == 1:
-    for row in range(5):
-        print("    ", end="")
-        for column in range(5):
-            if (column == 4) or (row == 0):
-                print(f" {sq} ", end="")
-            elif (row == column):
-                print(f" {sq} ", end="")
-            else:
-                print(end="   ")
-        print()
-else:
-    print("Warning! Enter only '1' Character.")
+
+r = 0
+c = 4
+for row in range(5):
+    print("    ", end="")
+    for column in range(5):
+        if (row == r and column == c):
+            print("*", end="  ")
+            r += 1
+            c -= 1
+        elif (row == 0 or column == 0):
+            print("*", end="  ")
+        else:
+            print(end="   ")
+    print()
