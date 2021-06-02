87e07c31cd517feec2b02a6cefb81b55ff6283cf
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-02 14:26:13 +0530

    Pattern 29 is added...

--- trial.py
+++ trial.py
@@ -1,12 +1,15 @@
 # String Methods...
 
-
+r = 0
+c = 4
 for row in range(5):
     print("    ", end="")
     for column in range(5):
-        if (column == 0 or row == 4):
+        if (row == r and column == c):
             print("*", end="  ")
-        elif (row == column):
+            r += 1
+            c -= 1
+        elif (column == 4 or row == 4):
             print("*", end="  ")
         else:
             print(end="   ")
