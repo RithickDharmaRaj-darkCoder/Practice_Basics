92ac6f463dac606648df7ed035d52b37d4e26f73
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-04 01:23:26 +0530

    added 2 patterns...

--- trial.py
+++ trial.py
@@ -1,29 +1,12 @@
-# String Methods...
+# Numerical Patterns ...
 
-a = "Rithick Dharma Raj"
-print(a,'\n'+"isspace() :",a.isspace())
-a = "    "
-print(a,'\n'+"isspace() :",a.isspace(),'\n--------------------')
 
-a = 'Rithick dharma Raj'
-print(a,'\n'+"istitle() :",a.istitle())
-a = "Rithick Dharma Raj"
-print(a,'\n'+"istitle() :",a.istitle(),'\n--------------------')
-
-a = "Rithick dharma RAJ"
-print(a,'\n'+"isupper() :",a.isupper())
-a = "17 RITHICK DHARMA RAJ *+-"
-print(a,'\n'+"istitle() :",a.isupper(),'\n--------------------')
-
-a = ("Rithick","Dharma","Raj")
-print(a,'\n'+"join() :","-".join(a),'\n--------------------')
-
-a = {'Fname':"Rithick",'Lname':"Dharma Raj"}
-print(a,'\n'+"formate_map() :","{Fname} is also called as "
-"{Lname}".format_map(a),'\n--------------------')
-
-a = "Rithick"
-print(a,"\n"+"ljust() :",a.ljust(10,'^'),".darkCoder",'\n--------------------')
-
-a = "Rithick"
-print(a,"\n"+"rjust() :",a.rjust(10,'*'),".darkCoder",'\n--------------------')
+k = 0
+for row in range(4):
+    print("    ",end=" ")
+    for space in range(4-row-1):
+        print(" ",end=" ")
+    for column in range(row+1):
+        print(k,end=" ")
+        k += 1
+    print()
