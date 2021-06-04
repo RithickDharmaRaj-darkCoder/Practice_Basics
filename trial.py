e48126532772f0bfe1efb9b3936aee8bf9a2c6b7
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-04 21:41:42 +0530

    43 and 44 string methods ...

--- trial.py
+++ trial.py
@@ -1,26 +1,6 @@
 # Numerical Patterns ...
 
-a = "Rithick Raj Rithick Dharma Raj"
-print(a,"\n"+"rpartition () :",a.rpartition("Rithick"),'\n--------------------')
+a = "Rithick"
+print(a,"\n"+"upper () :",a.upper(),'\n--------------------')
 
-a = "Rithick, Dharma, Raj, 17"
-print(a,"\n"+"rsplit () :",a.rsplit(", ",1))
-print("rsplit () :",a.rsplit(", ",2))
-print("rsplit () :",a.rsplit(", "),'\n--------------------')
-
-print(a,"\n"+"split () :",a.split(", ",1))
-print("split () :",a.split(", ",2))
-print("split () :",a.split(", "),'\n--------------------')
-
-a = "Rithick \nDharma \nRaj"
-print(a,'\n'+"splitlines () :",a.splitlines())
-print("splitlines () :",a.splitlines(False))
-print("splitlines () :",a.splitlines(True),'\n--------------------')
-
-a = "Rithick dharma raj"
-print(a,'\n'+"startswith () :",a.startswith("Rithick"))
-print(a,'\n'+"startswith () :",a.startswith("d",8),'\n--------------------')
-
-print(a,'\n'+"swapcase () :",a.swapcase(),'\n--------------------')
-
-print(a,'\n'+"title () :",a.title(),'\n--------------------')
+print(a,"\n"+"zfill () :",a.zfill(17),'\n--------------------')
