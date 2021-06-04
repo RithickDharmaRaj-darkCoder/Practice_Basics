500f9fcb0440a3c7704ef0330b8c550a2a4b547b
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-04 14:18:42 +0530

    26 - 35 string methods ...

--- trial.py
+++ trial.py
@@ -1,12 +1,26 @@
 # Numerical Patterns ...
 
+a = "Rithick dharma RAJ"
+print(a,'\n'+"lower() :",a.lower(),"\n--------------------")
 
-k = 0
-for row in range(4):
-    print("    ",end=" ")
-    for space in range(4-row-1):
-        print(" ",end=" ")
-    for column in range(row+1):
-        print(k,end=" ")
-        k += 1
-    print()
+a = "*     Rithick     *"
+print(a,'\n'+"lstrip() :",a.lstrip("* "))
+print("rstrip() :",a.rstrip("* "))
+print("strip() :",a.strip("* "),"\n--------------------")
+
+a = "RITHICK dHARMA RAJ"
+b = a.maketrans("d","D")
+print(a,'\n'+"maketrans() & translate() :",a.translate(b),"\n--------------------")
+
+a = "Rithick Dharma Raj"
+print(a,'\n'+"partition() :",a.partition("Dharma"),"\n--------------------")
+
+a = "Raj Raj Cholan"
+print(a,'\n'+"replace() :",a.replace("Raj","Raja",1))
+print(a,'\n'+"replace() :",a.replace("Raj","Raja",2))
+print(a,'\n'+"replace() :",a.replace("Raj","Raja"),"\n--------------------")
+
+a = "Rithick Raj, Dharma Raj"
+print(a,'\n'+"rfind() :",a.rfind("Raj"),"\n--------------------")
+
+print(a,'\n'+"rindex() :",a.rindex("Raj"),"\n--------------------")
