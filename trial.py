30f6337ec41d36eb035ac872f59f2fe9b4925e64
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-04 21:37:22 +0530

    36 - 42 string methods ...

--- trial.py
+++ trial.py
@@ -1,26 +1,26 @@
 # Numerical Patterns ...
 
-a = "Rithick dharma RAJ"
-print(a,'\n'+"lower() :",a.lower(),"\n--------------------")
+a = "Rithick Raj Rithick Dharma Raj"
+print(a,"\n"+"rpartition () :",a.rpartition("Rithick"),'\n--------------------')
 
-a = "*     Rithick     *"
-print(a,'\n'+"lstrip() :",a.lstrip("* "))
-print("rstrip() :",a.rstrip("* "))
-print("strip() :",a.strip("* "),"\n--------------------")
+a = "Rithick, Dharma, Raj, 17"
+print(a,"\n"+"rsplit () :",a.rsplit(", ",1))
+print("rsplit () :",a.rsplit(", ",2))
+print("rsplit () :",a.rsplit(", "),'\n--------------------')
 
-a = "RITHICK dHARMA RAJ"
-b = a.maketrans("d","D")
-print(a,'\n'+"maketrans() & translate() :",a.translate(b),"\n--------------------")
+print(a,"\n"+"split () :",a.split(", ",1))
+print("split () :",a.split(", ",2))
+print("split () :",a.split(", "),'\n--------------------')
 
-a = "Rithick Dharma Raj"
-print(a,'\n'+"partition() :",a.partition("Dharma"),"\n--------------------")
+a = "Rithick \nDharma \nRaj"
+print(a,'\n'+"splitlines () :",a.splitlines())
+print("splitlines () :",a.splitlines(False))
+print("splitlines () :",a.splitlines(True),'\n--------------------')
 
-a = "Raj Raj Cholan"
-print(a,'\n'+"replace() :",a.replace("Raj","Raja",1))
-print(a,'\n'+"replace() :",a.replace("Raj","Raja",2))
-print(a,'\n'+"replace() :",a.replace("Raj","Raja"),"\n--------------------")
+a = "Rithick dharma raj"
+print(a,'\n'+"startswith () :",a.startswith("Rithick"))
+print(a,'\n'+"startswith () :",a.startswith("d",8),'\n--------------------')
 
-a = "Rithick Raj, Dharma Raj"
-print(a,'\n'+"rfind() :",a.rfind("Raj"),"\n--------------------")
+print(a,'\n'+"swapcase () :",a.swapcase(),'\n--------------------')
 
-print(a,'\n'+"rindex() :",a.rindex("Raj"),"\n--------------------")
+print(a,'\n'+"title () :",a.title(),'\n--------------------')
