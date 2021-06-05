da179f235c3cac8ca73c1da2ef8391a3da1b0d66
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-05 15:24:52 +0530

    List datatype...

--- trial.py
+++ trial.py
@@ -1,7 +1,18 @@
-# Range Datatype ...
+# List Datatype ...
 
-for row in range(5):
-    print("     ", end=" ")
-    for column in range(row + 1):
-        print('*', end=" ")
-    print()
+lst1 = ["Rithick","Dharma","Raj"]
+lst2 = lst1.copy()
+print('Normal List :',lst1,'\n'+"copy :",lst2,'\n--------------------')
+
+lst3 = ['raja','raja','cholan']
+print("count () :",lst3.count('raja'),'\n--------------------')
+
+print("index () :",lst1.index("Dharma"),'\n--------------------')
+
+lst1.reverse()
+print("reverse () :",lst1,'\n--------------------')
+
+lst1.sort()
+print("sort (Ascending) :",lst1)
+lst1.sort(reverse=True)
+print("sort (Descending) :",lst1,'\n--------------------')
