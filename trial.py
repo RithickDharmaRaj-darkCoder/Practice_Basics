7d49464a78f638c9f1f37d0a15967235d9e45815
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-05 20:12:37 +0530

    Tuple datatype...

--- trial.py
+++ trial.py
@@ -1,14 +1,4 @@
-# List Datatype ...
+# Tuple Datatype ...
 
-lst1 = ["Rithick","Dharma","Raj"]
-lst2 = ["Rithick","Dharma","Raj"]
-print("Normal list () :",lst1,'\n--------------------')
-
-lst1.remove("Raj")
-print("remove () :",lst1,'\n--------------------')
-
-lst2.pop(1)
-print("pop () :",lst2,'\n--------------------')
-
-lst1.clear()
-print("clear () :",lst1,'\n--------------------')
+tpl = ("Raja","Raja","Cholan")
+print(tpl,'\n',type(tpl))
