c04c679722230c8299c3520388314647d82e7a9d
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-05 15:38:49 +0530

    List datatype...

--- trial.py
+++ trial.py
@@ -1,18 +1,14 @@
 # List Datatype ...
 
 lst1 = ["Rithick","Dharma","Raj"]
-lst2 = lst1.copy()
-print('Normal List :',lst1,'\n'+"copy :",lst2,'\n--------------------')
+lst2 = ["Rithick","Dharma","Raj"]
+print("Normal list () :",lst1,'\n--------------------')
 
-lst3 = ['raja','raja','cholan']
-print("count () :",lst3.count('raja'),'\n--------------------')
+lst1.remove("Raj")
+print("remove () :",lst1,'\n--------------------')
 
-print("index () :",lst1.index("Dharma"),'\n--------------------')
+lst2.pop(1)
+print("pop () :",lst2,'\n--------------------')
 
-lst1.reverse()
-print("reverse () :",lst1,'\n--------------------')
-
-lst1.sort()
-print("sort (Ascending) :",lst1)
-lst1.sort(reverse=True)
-print("sort (Descending) :",lst1,'\n--------------------')
+lst1.clear()
+print("clear () :",lst1,'\n--------------------')
