060c51f3decded23e0f222f594a77a14d402fa18
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-07 00:08:12 +0530

    Set datatype...

--- trial.py
+++ trial.py
@@ -1,27 +1,13 @@
 # Set Datatype ...
 
 a = {"Rithick","Dharma","Raj"}
-b = {"Rithick",".darkCoder"}
-print("Normal Set :",a)
-print("Normal Set :",b,'\n--------------------')
+c = {"1,2,3,4,5"}
+print("Normal Set :",a,"\n--------------------")
+b = a.copy()
+print("copy () :",b,"\n--------------------")
 
-c = a.difference(b)
-d = b.difference(a)
-print("difference () :",c)
-print("difference () :",d,'\n--------------------')
+d = a.union(c)
+print("union () :",d,"\n--------------------")
 
-a.difference_update(b)
-b.difference_update(a)
-print("difference_update () :",a)
-print("difference-update () :",b,'\n--------------------')
-
-a.discard("Raj")
-print("discord () :",a,'\n--------------------')
-
-s = {1,2,3,4,5,6}
-t = {4,5,6,7,8}
-e = t.intersection(s)
-print("intersection () :",e,'\n--------------------')
-
-t.intersection_update(s)
-print("intersection_update () :",t,'\n--------------------')
+a.update(c)
+print("update () :",a,"\n--------------------")
