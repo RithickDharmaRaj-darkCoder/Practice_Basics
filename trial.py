ce5aca5a93d7b5c137fa76a4777141568497efd4
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-10 01:04:17 +0530

    Dictionary datatype...

--- trial.py
+++ trial.py
@@ -1,5 +1,26 @@
 # Dictionary Datatype ...
 
-a = { 1:'One',2:"two",3:'THREE'}
-print(f'Normal Dictionary : {a}\n{type(a)}')
+a = {'Fname':'Rithick','hobby':'coding'}
+print(f'Normal Dictionary : {a}'
+      f'\n--------------------')
 
+b = a.get('Fname')
+print(f'get () : {b}')
+b = a.get('Lname','Dharma Raj')
+print(f'get () : {b}\n--------------------')
+
+d = a.keys()
+a['relax'] = 'Games'
+print(f'Normal Dictionary () : {a}\n'
+      f'keys () : {d}\n--------------------')
+
+a = ('Dept1','Dept2','Dept3')
+b = ('CSE')
+c = dict.fromkeys(a,b)
+print(f'fromkeys () : {c}\n--------------------')
+
+d = c.items()
+print(f'items () : {d}\n--------------------')
+
+d = c.copy()
+print(f'copy () : {d}\n--------------------')
