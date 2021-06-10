3c2944a18063df252ad52179f986e6f65899ca5e
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-10 23:16:45 +0530

    Pattern 32 is added...

--- trial.py
+++ trial.py
@@ -1,22 +1,11 @@
-# Dictionary Datatype ...
+# Diamond Pattern...
 
-a = {'Fname':'Rithick','hobby':'coding'}
-print(f'Normal Dictionary : {a}'
-      f'\n--------------------')
-b = a.values()
-print(f'values () : {b}\n--------------------')
-
-a.update({'Lname':'Dharma Raj'})
-print(f'update () : {a}\n--------------------')
-
-b = a.setdefault('Fname','RDR')
-print(f'setdefault () : {b}\n--------------------')
-
-a.pop('hobby')
-print(f'pop () : {a}\n--------------------')
-
-a.popitem()
-print(f'popitems () : {a}\n--------------------')
-
-a.clear()
-print(f'clear () : {a}\n--------------------')
+str = list(input("Enter your Name : ").upper())
+len_of_str = len(str)
+for row in range(len_of_str):
+    print(' ',end=" ")
+    for space in range():
+        print(" ",end=" ")
+    for column in range(row + 1):
+        print(str[column],end=" ")
+    print()
