6963c1e48cd5fac29778d9e5aa782603265762d5
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-11 00:00:33 +0530

    Pattern 35 is added...

--- trial.py
+++ trial.py
@@ -1,11 +1,2 @@
 # Diamond Pattern...
 
-str = list(input("Enter your Name : ").upper())
-len_of_str = len(str)
-for row in range(len_of_str):
-    print(' ',end=" ")
-    for space in range(len_of_str-row-1):
-        print(" ",end=" ")
-    for column in range(row + 1):
-        print(str[column],end=" ")
-    print()
