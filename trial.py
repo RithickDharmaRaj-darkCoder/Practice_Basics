9321c04622ee64bfd60d445a4ad9eee7889b265f
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-12 01:26:51 +0530

    Selection sort is added...

--- trial.py
+++ trial.py
@@ -1,17 +1,20 @@
 # Selection Sort...
 
 lst = []
-lsize = int(input("\nEnter how many elements to be insert : "))
-for i in range(lsize):
-    elst = int(input("Enter the elements to add it in list : "))
-    lst.append(elst)
-print("Before Selection Sort : ", lst)
+lsize = int(input("Enter how many numbers want to add : "))
+i = 1
+for call in range(lsize):
+    iput = int(input(f'Enter the number {i} : '))
+    lst.append(iput)
+    i += 1
+print(f'Before Selection Sort : {lst}')
 
-for outer in range(len(lst) - 1):
+for outer in range(len(lst)):
     min_index = outer
-    for inner in range(outer + 1, len(lst)):
+    for inner in range(outer+1,len(lst)):
         if lst[min_index] > lst[inner]:
             min_index = inner
     if lst[outer] != lst[min_index]:
-        lst[outer], lst[min_index] = lst[min_index], lst[outer]
-print("After Selection Sort : ", lst)
+        lst[min_index],lst[outer] = lst[outer],lst[min_index]
+    print(f'it{outer} : {lst}')
+print(f'After Selection Sort : {lst}')
