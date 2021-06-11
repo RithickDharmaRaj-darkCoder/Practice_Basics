04c150e6d4c2448edb4c050f17cd63f465f851cb
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-11 15:59:14 +0530

    Bubble sort is added...

--- trial.py
+++ trial.py
@@ -1,2 +1,16 @@
-# Diamond Pattern...
+# Bubble Sort...
 
+size = int(input("How many numbers want to insert : "))
+lst1 = []
+for iput in range(size):
+    lst2 = int(input("Enter the number : "))
+    lst1.append(lst2)
+
+print(f'\nOriginal lst : {lst1}\n')
+for repeat in range(len(lst1)-1):
+    for num in range(len(lst1)-1):
+        if lst1[num] > lst1[num + 1]:
+            lst1[num],lst1[num + 1] = lst1[num + 1],lst1[num]
+    print(f'Iteration {repeat+1} : {lst1}')
+
+print(f'\nBubble Sort :{lst1}')
