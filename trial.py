b2df94c170b4865c8d07eeefe0d6c24f237b0f25
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-13 03:09:27 +0530

    Merge Sort Added...

--- trial.py
+++ trial.py
@@ -1,16 +1,34 @@
-# Insertion Sort...
+# Merge Sort...
 
+def mergesort(lst):
+    if len(lst) > 1:
+        mid = len(lst) // 2
+        lft_lst = lst[:mid]
+        right_lst = lst[mid:]
+        mergesort(lft_lst)
+        mergesort(right_lst)
 
-lsize = int(input("\nEnter how many elements to be insert : "))
-lst = [int(input(f'Add element {i+1} : ')) for i in range(lsize)]
-print(f'Before Insertion Sort : {lst}')
+        (i,j,k) = (0,0,0)
+        while len(lft_lst) > i and len(right_lst) > j:
+            if lft_lst[i] < right_lst[j]:
+                lst[k] = lft_lst[i]
+                i += 1
+                k += 1
+            else:
+                lst[k] = right_lst[j]
+                j += 1
+                k += 1
 
-
-for index in range(1,len(lst)):
-    curent_value = lst[index]
-    postion = index
-    while (curent_value < lst[postion-1] ) and (postion > 0):
-        lst[postion] = lst[postion-1]
-        postion -= 1
-    lst[postion] = curent_value
-print(f'After Insertion Sort : {lst}')
+        while len(lft_lst) > i:
+            lst[k] = lft_lst[i]
+            i += 1
+            k += 1
+        while len(right_lst) > j:
+            lst[k] = right_lst[j]
+            j += 1
+            k += 1
+lsize = int(input('How many numbers want to add : '))
+lst = [int(input(f'Add number {i+1} : '))for i in range(lsize)]
+print(f'Before Merge Sort : {lst}')
+mergesort(lst)
+print(f'After Merge Sort : {lst}')
