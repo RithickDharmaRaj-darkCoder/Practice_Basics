28c5c657cbdfa95b8272f672ea56b71d02c419b9
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-16 01:11:32 +0530

    Quick Sort Added...

--- trial.py
+++ trial.py
@@ -1,2 +1,25 @@
 # Quick Sort...
 
+lsize = int(input('How many numbers want to insert : '))
+lst = [int(input(f'Add Number {i + 1} : ')) for i in range(lsize)]
+print(f'Before Quick Sort : {lst}')
+
+def quick(lst):
+    n = len(lst)
+    if n <= 1:
+        return lst
+    else:
+        key = lst.pop()
+
+    lower_elements = []
+    greater_elements = []
+
+    for element in lst:
+        if element < key:
+            lower_elements.append(element)
+        else:
+            greater_elements.append(element)
+
+    return quick(lower_elements) + [key] + quick(greater_elements)
+
+print(f'After Quick Sort  : {quick(lst)}')
