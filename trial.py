b685037d2bd4d365809cf97c394a6b96eafebf5c
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-17 01:21:51 +0530

    Shell Sort Added...

--- trial.py
+++ trial.py
@@ -1,15 +1,2 @@
-#  Sort...
+# Shell Sort...
 
-lst = [7,8,5,6,9]
-lst.sort()
-print(lst)
-print(sorted(lst))
-
-tpl = (7,8,5,6,8)
-print(sorted(tpl))
-
-set = {7,8,5,6,9}
-print(sorted(set))
-
-dict = {'one':1,'two':2,'three':3}
-print(sorted(dict.items()))
