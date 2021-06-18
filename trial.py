44525f2cb73fa8d246c893fd07fca27745fa2fc6
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-19 03:22:54 +0530

    Data Structure >> User Defined >> Queue >> Collections Lib >> deque() ...

--- trial.py
+++ trial.py
@@ -1,24 +1,28 @@
 # Data Structure ...
 # User-Defined ...
 # Queue ...
-# List as Queue ...
+# Collection Lib ...
+# deque ...
 
-qu = []
-qu.insert(0,7)
-qu.insert(0,8)
-qu.insert(0,5)
-print(qu)
-qu.pop()
-qu.pop()
-qu.pop()
-print(qu)
-print(not qu)
+from collections import *
 
-qu = []
-qu.insert(0,7)
-qu.insert(0,8)
-qu.insert(0,5)
-print(qu)
-qu.clear()
-print(qu)
+qu = deque()
 print(not qu)
+qu.appendleft(7)
+qu.appendleft(8)
+qu.appendleft(5)
+qu.appendleft(6)
+qu.appendleft(9)
+print(qu)
+qu.pop()
+print(qu)
+
+qu1 = deque()
+qu1.append(7)
+qu1.append(8)
+qu1.append(5)
+qu1.append(6)
+qu1.append(9)
+print(qu1)
+qu1.popleft()
+print(qu1[0])
