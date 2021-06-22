e85fb2011112f9fa744e48ada42093d9b5ababdb
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-23 00:32:36 +0530

    Classes and Objects ...

--- trial.py
+++ trial.py
@@ -1,18 +1,18 @@
-# Data Structure ...
-# User-Defined ...
-# Priority Queue ...
-# queue Lib ...
-# PriorityQueue ...
+# Class And Object ...
 
-from queue import *
+class StudentInfo:
+    def __init__(self,stdname,dept,year,regno):
+        self.name = stdname
+        self.dept = dept
+        self.year = year
+        self.rollno = regno
+        print('Data Saved Successfully!',end=" ")
+    def display(self):
+        print(f'Hi, i\'m {self.name} studying {self.year} year-{self.dept} and my register number is {self.rollno}.')
 
-qu = PriorityQueue()
-qu.put(7)
-qu.put(8)
-qu.put(5)
-qu.put(6)
-qu.put(9)
-print(qu)
+StudentInfo('Rithick Dharma Raj','CSE','Final',regno=6128_17_104_008)
+StudentInfo('Rithick Dharma Raj','CSE','Final',regno=6128_17_104_008).display()
 
-print(qu.get())
-print(qu.get())
+std1 = StudentInfo('Rithick Dharma Raj','CSE','Final',regno=6128_17_104_008)
+std1.display()
+std2 = StudentInfo('Dharma Raj','ECE','Final',regno=6128_17_106_000).display()
