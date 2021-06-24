fb1eecf36299a8b61ed42100289e11b46d3381ab
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-25 01:08:58 +0530

    Multiline Commend ...

--- trial.py
+++ trial.py
@@ -1,5 +1,5 @@
 # Class And Object ...
-
+'''
 class StudentInfo():
     college = 'vvit'
     def __init__(self,stdname,dept,year,regno):
@@ -8,12 +8,12 @@ class StudentInfo():
         self.year = year
         self.rollno = regno
     def stdinclg(self):
-        print(self.name,self.dept,self.year,self.rollno)
-
+        print(self.name,self.dept,self.year,self.rollno)'''
+"""
 class StudentInfo(StudentInfo):
 
     def stdinhome(self):
         print(self.name,self.dept,self.college)
 
 StudentInfo('Mr_17','CSE','Final',6128_17_104_008).stdinclg()
-StudentInfo('Mr_17','CSE','Final',00).stdinhome()
+StudentInfo('Mr_17','CSE','Final',00).stdinhome()"""
