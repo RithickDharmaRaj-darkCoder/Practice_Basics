0c2da6796c026d9d3c552b3eb89c83401fd1050d
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-26 04:18:55 +0530

    Adding data @ begining of Linked List ...

--- trial.py
+++ trial.py
@@ -1,9 +1,9 @@
 # Data Structure ...
 # User-Defined ...
 # Linked List ...
-# Singly Linked List (Traversal Working !!!) ...
+# Singly Linked List (Adding data @ begining) ...
 
-class node():
+class creatingnode():
     def __init__(self,data):
         self.data = data
         self.linkto = None
@@ -14,12 +14,22 @@ class linkedlist():
 
     def traversal(self):
         if self.head is None:
+            print(f'[ Head --]--> {self.head}')
             print('Linked List is Empty!')
         else:
-            node = self.head
-            while node is not None:
-                print(node.data)
-                node = node.linkto
+            n = self.head
+            while n is not None:
+                print(f'[ [{n.data}] | [{n.linkto}] ] ')
+                n = n.linkto
 
-node1 = linkedlist()
-node1.traversal()
+    def add_at_starting(self,data):
+        newnode = creatingnode(data)
+        newnode.linkto = self.head
+        self.head = newnode
+        print(f'[ Head --]--> {self.head}')
+
+ll = linkedlist()
+ll.add_at_starting(50)
+ll.traversal()
+ll.add_at_starting(30)
+ll.traversal()
