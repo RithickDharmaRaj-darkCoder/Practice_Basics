c8646cb3f6e1ca000ec6b625c3ab9e06c6a7d950
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-26 05:04:37 +0530

    Adding data @ Ending of Linked List ...

--- trial.py
+++ trial.py
@@ -1,7 +1,7 @@
 # Data Structure ...
 # User-Defined ...
 # Linked List ...
-# Singly Linked List (Adding data @ begining) ...
+# Singly Linked List (Adding data @ Ending) ...
 
 class creatingnode():
     def __init__(self,data):
@@ -14,22 +14,30 @@ class linkedlist():
 
     def traversal(self):
         if self.head is None:
-            print(f'[ Head --]--> {self.head}')
             print('Linked List is Empty!')
         else:
             n = self.head
             while n is not None:
                 print(f'[ [{n.data}] | [{n.linkto}] ] ')
                 n = n.linkto
+            print('--------------------')
 
     def add_at_starting(self,data):
         newnode = creatingnode(data)
         newnode.linkto = self.head
         self.head = newnode
-        print(f'[ Head --]--> {self.head}')
+
+    def add_at_ending(self,data):
+        newnode = creatingnode(data)
+        if self.head is None:
+            self.head = newnode
+        else:
+            n = self.head
+            while n.linkto is not None:
+                n = n.linkto
+            n.linkto = newnode
 
 ll = linkedlist()
-ll.add_at_starting(50)
-ll.traversal()
-ll.add_at_starting(30)
+ll.add_at_starting(10)
+ll.add_at_ending(15)
 ll.traversal()
