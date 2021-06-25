5c0de07e143dab47f5b9c5c0d9920e43ef082621
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-25 23:33:51 +0530

    Encapsulation private method ...

--- trial.py
+++ trial.py
@@ -1,19 +1,15 @@
 # Class And Object ...
-'''
-class StudentInfo():
-    college = 'vvit'
-    def __init__(self,stdname,dept,year,regno):
-        self.name = stdname
-        self.dept = dept
-        self.year = year
-        self.rollno = regno
-    def stdinclg(self):
-        print(self.name,self.dept,self.year,self.rollno)'''
-"""
-class StudentInfo(StudentInfo):
 
-    def stdinhome(self):
-        print(self.name,self.dept,self.college)
+class students():
 
-StudentInfo('Mr_17','CSE','Final',6128_17_104_008).stdinclg()
-StudentInfo('Mr_17','CSE','Final',00).stdinhome()"""
+    def __init__(self,name,roll):
+        self.name = name
+        self.roll = roll
+    def info(self):
+        print(self.name)
+        self.__rollno()
+    def __rollno(self):
+        print(self.roll)
+
+std1 = students('RDR','007')
+
