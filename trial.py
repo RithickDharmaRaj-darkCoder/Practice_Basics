54688fa2b29c71b0293632f2af8a1502628fa63a
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-30 04:07:54 +0530

    Adding a node after the particular node...

--- trial.py
+++ trial.py
@@ -71,3 +71,56 @@ class D_LinkedList():
         else:
             self.add_at_starting(data)
 
+    def add_after_node(self,data,x):
+        if self.head is None:
+            print('Linked List is Empty!')
+        else:
+            h = self.head
+            while h is not None:
+                if h.data == x and h.linktoN == None:
+                    self.add_at_ending(data)
+                    break
+                elif h.data != x and h.linktoN is not None:
+                    h = h.linktoN
+                elif h.data != x and h.linktoN is None:
+                    print(f'{x} is not in the Linked List!')
+                    break
+                elif h.data == x and h.linktoN is not None:
+                    newnode = create_node(data)
+                    newnode.linktoN = h.linktoN
+                    h.linktoN = newnode
+                    newnode.linktoP = newnode.linktoN.linktoP
+                    newnode.linktoN.linktoP = newnode
+                    break
+
+    def add_before_node(self,data,x):
+        if self.head is None:
+            print('Linked List is Empty!')
+        else:
+            t = self.tail
+            while t is not None:
+                if t.data == x and t.linktoP is None:
+                    self.add_at_starting(data)
+                    break
+                elif t.data != x and t.linktoP is not None:
+                    t = t.linktoP
+                elif t.data != x and t.linktoP is None:
+                    print(f'{x} is not in the Linked List!')
+                    break
+                elif t.data == x and t.linktoP is not None:
+                    newnode = create_node(data)
+                    newnode.linktoP = t.linktoP
+                    t.linktoP = newnode
+                    newnode.linktoN = newnode.linktoP.linktoN
+                    newnode.linktoP.linktoN = newnode
+                    break
+
+
+
+
+ll1 = D_LinkedList('LL1')
+
+
+
+ll1.traversal_Forword()
+ll1.traversal_Backword()
