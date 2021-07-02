4230149cd26b8d4f289679e165e4e20c57441288
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-07-03 04:15:59 +0530

    Started Tkinter ...

--- copy.py
+++ copy.py
@@ -4,35 +4,3 @@
 # by getting inputs from the user.def disply():
 
 
-'''class aadhaarCardManagement:
-    option1 = '[1. Add Member]'
-    option2 = '[2. View Member Detail]'
-    option3 = '[3. Quit]'
-    def option(self):
-        print(self.option1,self.option2,self.option3)
-        option = input("Enter (1/2/3) : ")
-        while option:
-            if option == '1':
-                self.addmember()
-                break
-            elif option == '2':
-                self.viewmember()
-                break
-            else:
-                break
-
-    def addmember(self):
-        print('******** Adding Member ********')
-        newmember = (name = input('Enter Your Name :'),age = int(input('Enter your Age : ')),Gender = input('Male/Female : '),id = 0,memdict={None})
-        print('Member added successfully!\n')
-        self.option()
-
-
-    def viewmember(self):
-        print('******** View Detail ********')
-        print()
-        self.option()
-
-mem1 = aadhaarCardManagement().option()'''
-
-
