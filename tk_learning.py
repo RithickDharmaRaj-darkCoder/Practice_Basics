9fc63829a980976523b8a7ef40b32ee30cc7928b
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-07-03 06:55:28 +0530

    First Tkinter Project ...

--- tk_learning.py
+++ tk_learning.py
@@ -1,1 +1,20 @@
 # Learning Tkinter ...
+
+from tkinter import *
+main = Tk()
+
+title = Label(main,text='***  AADHAAR MANAGEMENT  ***')
+title.pack()
+
+name = Entry(main)
+name.pack()
+name.insert(2,'Enter Your Name...')
+
+def clickme():
+    label = Label(main,text=f'Hi! {name.get()}, u have successfully submitted ur data.')
+    label.pack()
+btn1 = Button(main,text='Sumbit',command=clickme)
+btn1.pack()
+
+
+main.mainloop()
