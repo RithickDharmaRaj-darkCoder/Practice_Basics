a74162c2e67b742f2db02ce0ea7f97ed585a530b
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-07-17 15:09:17 +0530

    Deleting Node & Edges in Adjacency List Representation Graph...

--- trial.py
+++ trial.py
@@ -24,7 +24,7 @@ class alrg:
             if v1 not in self.graph[v2]:
                 self.graph[v2].append(v1)
 
-    '''def delete_node(self,v):
+    def delete_node(self,v):
         if v not in self.graph:
             print(f'{v} is not in graph!')
         else:
@@ -42,7 +42,7 @@ class alrg:
             print(f'{v2} is not in graph!')
         else:
             self.graph[v1].remove(v2)
-            self.graph[v2].remove(v1)'''
+            self.graph[v2].remove(v1)
 
 
 
