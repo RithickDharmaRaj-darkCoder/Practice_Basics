3446067ab96cbec48aeb4a3d2a76f224a158515a
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-07-18 01:39:37 +0530

    Deleting weighted (Node & Edges) in Adjacency List Representation Graph...

--- trial.py
+++ trial.py
@@ -13,27 +13,29 @@ class alrg:
         else:
             self.graph[v] = []
 
-    def add_edges(self,v1,v2):
+    def add_edges(self,v1,v2,w):
         if v1 not in self.graph:
             print(f'{v1} is not in graph!')
         elif v2 not in self.graph:
             print(f'{v2} is not in graph!')
         else:
+            lst1 = [v2,w]
+            lst2 = [v1,w]
             if v2 not in self.graph[v1]:
-                self.graph[v1].append(v2)
+                self.graph[v1].append(lst1)
             if v1 not in self.graph[v2]:
-                self.graph[v2].append(v1)
+                self.graph[v2].append(lst2)
 
     def delete_node(self,v):
         if v not in self.graph:
             print(f'{v} is not in graph!')
         else:
             self.graph.pop(v)
-            for i in self.graph:
-                try:
-                    self.graph[i].remove(v)
-                except ValueError:
-                    pass
+            for keys in self.graph:
+                for outer in range(len(self.graph[keys])):
+                    if self.graph[keys][outer][0] == v:
+                        self.graph[keys].pop(outer)
+
 
     def delete_edge(self,v1,v2):
         if v1 not in self.graph:
@@ -41,8 +43,17 @@ class alrg:
         elif v2 not in self.graph:
             print(f'{v2} is not in graph!')
         else:
-            self.graph[v1].remove(v2)
-            self.graph[v2].remove(v1)
+            #self.graph[v1].remove(v2)
+            #self.graph[v2].remove(v1)
+            for outer in range(len(self.graph[v1])):
+                if self.graph[v1][outer][0] == v2:
+                    del self.graph[v1][outer]
+            for out in range(len(self.graph[v2])):
+                try:
+                    if self.graph[v2][out][0] == v1:
+                        del self.graph[v2][out]
+                except IndexError:
+                    pass
 
 
 
@@ -55,13 +66,13 @@ gp.add_node("D")
 gp.add_node("E")
 
 # Adding Edges ...
-gp.add_edges("A","B")
-gp.add_edges("A","C")
-gp.add_edges("A","D")
-gp.add_edges("B","D")
-gp.add_edges("B","E")
-gp.add_edges("C","D")
-gp.add_edges("D","E")
+gp.add_edges("A","B",2)
+gp.add_edges("A","C",3)
+gp.add_edges("A","D",1)
+gp.add_edges("B","D",1)
+gp.add_edges("B","E",4)
+gp.add_edges("C","D",1)
+gp.add_edges("D","E",4)
 
 # Deleting Nodes ...
 gp.delete_node('E')
