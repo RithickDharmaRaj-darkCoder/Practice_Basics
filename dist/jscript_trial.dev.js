5c0864f65bd028b1d86ee4c4d12180f9dbd6dcd1
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-07-21 23:34:28 +0530

    Variable Decleration in JS ...

--- dist/jscript_trial.dev.js
+++ dist/jscript_trial.dev.js
@@ -1,12 +1,12 @@
+"use strict";
+
 // JavaScript Learning Field
+
 /*
 alert("Are you sure to open this page? ")
 */
-
 // Variables Declaration ...
 var num1 = 17;
 var num2 = 17;
-var Result = (num1===num2)?(num2+num1):num2+num1;
-
-document.write("Result : "+Result);
-
+var Result = num1 === num2 ? num2 + num1 : num2 + num1;
+document.write("Result : " + Result);
